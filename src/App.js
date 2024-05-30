7cf7ccca72fd66073dabd0b42ad1c274c92e9343
Karthik <karthik@Pro.local>
2024-05-30 12:28:46 +0530

    version 1.01  Please enter the commit message for your changes. Lines starting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [isrunning,setisrunning]=useState(false)
+  function handler() {
+    setisrunning(!isrunning)
+    console.log("is running")
+    
+  }
+  function handle() {
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>jikan o tomare</h1>
+      <div>
+        <button className='buuton' onClick={handler}>
+          {isrunning ?"start":"stop"}
+        </button>
+        <button classname='buuton' onClick={handle}>
+        reset
+        </button>
+      </div>
     </div>
   );
 }
